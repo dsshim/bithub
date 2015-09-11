ecf3d1f6662f2b2eafbdb7cf887f517e6e5d0aed
dsshim <daveshim@gmail.com>
2015-09-11 11:15:38 -0600

    adds styling changes to homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
     user = User.find_or_create_from_auth(oauth_data)
     if user
       session[:user_id] = user.id
+      flash[:success] = "Welcome Back #{current_user.nickname}!"
       redirect_to dashboard_path
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = "You have successfully logged out!"
     redirect_to root_path
   end
 
